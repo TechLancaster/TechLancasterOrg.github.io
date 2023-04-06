d4cbd7e6107877aa59779e6fcce661ddba3586eb
Luke Demi <lddemi@gmail.com>
2023-04-06 11:07:57 -0400

    add the join email button + prettier formatting

--- content/_index.md
+++
@@ -1,5 +0,0 @@
-Tech Lancaster is a social meetup of people who are interested in technology and want to learn more about it. The group is open to everyone regardless of experience or background.
-
-Our namesake meetup "Tech Lancaster Meetup" typically has two presentations surrounded by social time. Generally these presentations contain topics are designed to provide a 30,000 foot view of a technology with maybe a dip or two into some actual code.
-
-If you're interested in a purely social meetup without presentations or structure, check out Pub Standards and Open Coffee Club. These are designed to be low pressure meetups that you can pop in or out of and meet other members of the community.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "layouts/**/*.html",
-  ],
+  content: ["layouts/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "gradient-to-r": "linear-gradient(to right, var(--tw-gradient-stops))",
+      }),
+      animation: {
+        bounce: "bounce 1s infinite",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  variants: {
+    extend: {
+      backgroundImage: ["responsive", "hover", "focus", "group-hover"],
+      animation: ["hover", "focus", "group-hover"],
+      scale: ["hover", "focus", "group-hover"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
